5b01dcfda70c6aad1b087a416e5a339e53029e90
Edwin Trigos <edwintrigos24@gmail.com>
2022-12-29 12:48:10 -0500

    creacion de vista de usuarios, configuracion de la tabla con datatable en usuarios.js

--- presentacion/vistas/usuarios.php
+++ presentacion/vistas/usuarios.php
@@ -3,6 +3,7 @@ session_start();
 if (!isset($_SESSION['usuario'])) header('location: ../../index.php?mensaje=Ya hay una sesion activa, acceso no autorizado'); //sesiones activas al tiempo
 else {
     $USUARIO = unserialize($_SESSION['usuario']);
+    $identificacion = $USUARIO->getIdentificacion();
 }
 
 //codigo en caso de que se generen mensajes desde la adicion, eliminacion o edicion de un usuario
@@ -12,62 +13,99 @@ if (isset($_REQUEST['mensaje'])) {
     $sms = "<div id='alerta' class='alert alert-danger text-center m-2 ' role='alert'>$mensaje</div>";
 }
 
-$lista = '';
+//$json_usuarios = Usuario::getListaEnObjetos(null, null);
+$json_usuarios = '[';
+$resultado = Usuario::getListaEnObjetos(null, null);
+for ($i = 0; $i < count($resultado); $i++) {
+    $usuario = $resultado[$i];
+    $json_usuarios .= '{ identificacion: "' . $usuario->getIdentificacion()
+        . '", nombre: "' . $usuario->getNombre()
+        . '", apellido: "' . $usuario->getApellido()
+        . '", tipoUsuario: "' . $usuario->getTipoUsuario()
+        . '", nombreUsuario: "' . $usuario->getNombreUsuario()
+        . '", correo: "' . $usuario->getCorreo()
+        . '", telefono: "' . $usuario->getTelefono()
+        . '", tipoIdentificacion: "' . $usuario->getTipoIdentificacion()
+        . '", foto: "' . $usuario->getFoto()
+        . '", direccion: "' . $usuario->getDireccion()
+        . '", nitEmpresa: "' . $usuario->getNitEmpresa()
+        . '"},';
+}
+$json_usuarios .= ']';
 
-$json_usuarios = Usuario::getListaEnObjetos(null, null);
-
-// for ($i = 0; $i < count($resultado); $i++) {
-//     $usuario = $resultado[$i];
-//     $lista .= '<tr>';
-//     $lista .= "<td>{$usuario->getIdentificacion()}</td>";
-//     $lista .= "<td>{$usuario->getNombre()}</td>";
-//     $lista .= "<td>{$usuario->getApellido()}</td>";
-//     $lista .= "<td>{$usuario->getTipoUsuario()}</td>";
-//     $lista .= "<td>{$usuario->getNombreUsuario()}</td>";
-//     $lista .= "<td>{$usuario->getCorreo()}</td>";
-//     $lista .= "<td>{$usuario->getTelefono()}</td>";
-//     $lista .= "<td>{$usuario->getTipoIdentificacion()}</td>";
-//     $lista .= "<td>{$usuario->getFoto()}</td>";
-//     $lista .= "<td>{$usuario->getDireccion()}</td>";
-//     $lista .= "<td>{$usuario->getNitEmpresa()}</td>";
-
-//     //$lista .= "<td><a href='principal.php?CONTENIDO=presentacion/configuracion/habilidad/habilidadFormulario.php&accion=adicionar'>Adicionar</a></td>";
-//     $lista .= "<td><a href='principal.php?CONTENIDO=presentacion/configuracion/usuario/usuarioFormulario.php&accion=Modificar&identificacion={$usuario->getIdentificacion()}' title='modificar usuario'> Modificar </a></td>";
-//     $lista .= "<td><a href='principal.php?CONTENIDO=presentacion/configuracion/usuario/usuarioCRUD.php&accion=Eliminar&identificacion={$usuario->getIdentificacion()}' onclick='eliminar({$usuario->getIdentificacion()})' title='Eliminar usuario'>Eliminar</a></td>";
-//     $lista .= "<td></td>";
-//     $lista .= "</tr>";
-// }
+print_r($json_usuarios);
 ?>
 
-<h3>LISTA DE USUARIOS</h3>
-<table border="1">
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Nombre</th>
-            <th>Apellidos</th>
-            <th>Tipo de usuario</th>
-            <th>Nombre de usuario en el sistema</th>
-            <th>Correo</th>
-            <th>Telefono</th>
-            <th>Tipo de identificacion del usuario</th>
-            <th>Foto</th>
-            <th>Direccion</th>
-            <th>Nit de empresa donde trabaja</th>
-            <th><a href='principal.php?CONTENIDO=presentacion/configuracion/usuario/usuarioFormulario.php&accion=Adicionar'>Adicionar</a></th>
-        </tr>
-    </thead>
-    <tbody>
-        <?= $json_usuarios ?>
-    </tbody>
-</table>
+<h3 class="text-center">GESTION DE USUSARIOS</h3>
+<?php
+if (Usuario::esAdmin($identificacion)) {
+    // deja manipular usuarios si el user de sesion es ADMIN 
+    // echo  '<span><button type="button" class="btn btn-primary"><a href="principal.php?CONTENIDO=presentacion/configuracion/proyecto/proyectoFormulario.php&accion=Adicionar"></a>Nuevo Proyecto</button></span> ';
+    // echo  '<button type="button" id="addRow" class="btn btn-primary">Nuevo Proyecto</button></span> ';
+}
+?>
 
+<fieldset class="form-group border p-3">
+    <div class="container col-auto justify-content-center">
+        <div class="row">
+            <legend class="w-auto px-2">Usuarios Registrados</legend>
 
-<script type="text/javascript">
-    function eliminar(id) {
-        var respuesta = confirm("Esta seguro de eliminar este registro?");
-        if (respuesta) {
-            location = "principal.php?CONTENIDO=presentacion/configuracion/usuario/usuarioCRUD.php&accion=Eliminar&identificacion=" + id;
-        }
+            <table id="tblUsuarios" class="table table-responsive table-striped table-borded dataTable-content" cellpacing="0" width="100%"></table>
+            <!-- <div class="col align-self-center">
+                <textarea id="campoDescripcion" type="text" class="form-control" style="min-width: 100%" rows="5" disabled="disabled"></textarea>
+            </div> -->
+
+            <table id="new-row-template" style="display:none" class="col-auto">
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td>__identificacion__</td>
+                        <td>__nombre__</td>
+                        <td>__apellido__</td>
+                        <td>__tipoUsuario__</td>
+                        <td>__nombre Usuario__</td>
+                        <td>__correo__</td>
+                        <td>__tipo identificacion__</td>
+                        <td><input type="file" name="foto" id=""></td>
+                        <td>__telefono__</td>
+                        <td>__direccion__</td>
+                        <td>__Empresa__</td>
+                        <td>
+                            <i class='bi ' +`${claseBotonEditarRow}` aria-hidden="true"></i>
+                            <i class='bi ' +`${claseBotonEliminarRow}` aria-hidden="true"></i>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!-- <br> 
+        <div class="row">
+            <div class="col align-self-start"></div>
+            <div class="col align-self-center"></div>
+            <div id="botonesGuardarCambios" class="col align-self-end" disabled="disabled">
+                <button type="button" id="btn-cancel" class="btn btn-secondary" data-dismiss="modal">Revertir Cambios</button>
+                <button type="button" id="btn-save" class="btn btn-primary" data-dismiss="modal">Guardar Cambios</button>
+            </div>
+        </div> -->
+    </div>
+
+</fieldset>
+
+<!-- <script type="text/javascript" src="assets/barraBusqueda.js"></script> -->
+<script type="module">
+    import {
+        cargarUsuarios
+    } from './presentacion/vistas/js/usuarios.js'
+
+    let lisUsuarios = [];
+    <?php echo 'const dProy = ' . $json_usuarios . ';'; ?>
+    console.log(dProy);
+    if (lisUsuarios.length == 0 || lisUsuarios == null) {
+        lisUsuarios = [...dProy];
     }
+    //genera_tabla(arreglo);    
+
+    $(document).ready(function() {
+        cargarUsuarios('tblUsuarios', lisUsuarios);
+    });
 </script>
