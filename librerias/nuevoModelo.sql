adbe924ecfbbb13ec0b9a35cbc0d19c9d7fb1c13
Edwin Trigos <edwintrigos24@gmail.com>
2023-01-03 23:45:49 -0500

    *correccion de los primeros 3 insert para poblar la base

--- librerias/nuevoModelo.sql
+++ librerias/nuevoModelo.sql
@@ -129,14 +129,13 @@ CREATE TABLE proyectos_habilidades (
 
 -- empresa
 -- UUID V4 - https://www.delftstack.com/howto/php/php-uuid/
-INSERT INTO devmanager2.empresas
+INSERT INTO empresas
 (id, nit, nombre, direccion, correo, telefono, nombre_representante, correo_representante)
 VALUES
 ('20a9d4e8-63a8-48f0-910f-c7339d8fd7ec', '333', 'aguas de bogota', 'Bogotá, Cundinamarca ', 'aguas.bogota@gmail.com', '33333333', 'Juan Alvarez', 'juan123@gmail.com'),
 ('b7f6046a-b834-48f0-856e-8a360b495406', '334', 'actses', 'Bucaramanga, Cundinamarca ', 'actses@gmail.com', '33333333', 'Juan Alvarez', 'aljuan@gmail.com');
 
--- usuarios
-INSERT INTO devmanager2.usuarios
+INSERT INTO usuarios
 (id, identificacion, tipo_identificacion, nombres, apellidos, correo, clave_hash, direccion, nombre_foto, telefono, tipo_usuario, id_empresa)
 VALUES
 ('499a9d4a-fbf1-4ea7-850b-01bf301a98af', '1098657073', 'C', 'William', 'Trigos', 'wtrigos@gmail.com', 'c4ca4238a0b923820dcc509a6f75849b', 'Provenza', 'fwilliam.jpg', '334422', 'D', '20a9d4e8-63a8-48f0-910f-c7339d8fd7ec'),
@@ -144,9 +143,7 @@ VALUES
 ('eb036f8a-75bd-4811-a477-1444e2521f3b', '10951', 'R', 'Edwin', 'Trigos', 'etrigos@gmail.com', 'c4ca4238a0b923820dcc509a6f75849b', 'provenza', 'fedwin.jpg', '313316', 'T', '20a9d4e8-63a8-48f0-910f-c7339d8fd7ec'),
 ('25c00e25-9042-4f04-b059-c34820b800f8', '10985', 'P', 'Pepito', 'Peréz', 'pper@aol.com', 'MD5(pepito)', 'maracay', 'fpepito.jpg', '039', 'T', 'b7f6046a-b834-48f0-856e-8a360b495406');
 
-
--- proyectos
-INSERT INTO devmanager2.proyectos
+INSERT INTO proyectos
 (id, nombre, descripcion, estado, fecha_inicio, fecha_fin, id_usuario)
 VALUES
 ('abfd9937-a08b-47b0-8b64-3338455d99f4','Proyecto para escuela', 'Software biblioteca de escuela', 'T', '2022-03-11 00:00:00', '2022-04-12 00:00:00', '499a9d4a-fbf1-4ea7-850b-01bf301a98af'),
@@ -154,26 +151,22 @@ VALUES
 ('bbfc7b5b-f77f-4a58-a3ce-5163bac61a4c','Aerolinea app movil ', 'Aplicación móvil para aerolínea ', 'E', '2020-03-11 00:00:00', '2022-12-31 00:00:00', '8fa903bc-0789-43b2-901b-70d6c60334ba'),
 ('f660bbbf-dd1a-4eab-9866-dba8092c94c5','Nequi plata infinita', 'Aplicacion movil', 'T', '2012-03-11 00:00:00', '2014-04-12 00:00:00', '499a9d4a-fbf1-4ea7-850b-01bf301a98af');
 
--- habilidades
 INSERT INTO habilidades (id,nombre,descripcion) 
 VALUES
 ('0463add9-313e-49bf-a07e-800612c36263','Javascript','Manejo Javascript'),
 ('65374dc5-692f-483d-9809-3371a7222a79','PHP','Manejo de PHP');
 
--- habilidades de un usuario
 INSERT INTO usuarios_habilidades (id,experiencia,id_usuario,id_habilidad)
 VALUES
 ('2fbaea0f-7171-4f8f-8615-27c4bc90f9f1','Autodidacta','eb036f8a-75bd-4811-a477-1444e2521f3b','65374dc5-692f-483d-9809-3371a7222a79'),
 ('e48a2ef4-bf5f-4d9b-8352-7f8067ea6809','Certificada','8fa903bc-0789-43b2-901b-70d6c60334ba','0463add9-313e-49bf-a07e-800612c36263');
 
--- trabajadores postulados/asignados a proyectos
 INSERT INTO rh_proyectos (id,fecha_solicitud,estado,id_proyecto,id_usuario) 
 VALUES
 ('39070ae4-a9f5-477b-aeb4-a28744b95776','2022-03-14','E','f660bbbf-dd1a-4eab-9866-dba8092c94c5','eb036f8a-75bd-4811-a477-1444e2521f3b'),
 ('4144ebe5-51d0-41f6-9c1d-1ce3917fb53c','2022-03-14','A','43a9245a-275a-4b23-8ac0-a63fefa13013','eb036f8a-75bd-4811-a477-1444e2521f3b'),
 ('95eb15b1-9912-44f9-963c-0635318dd7fa','2022-03-14','E','f660bbbf-dd1a-4eab-9866-dba8092c94c5','8fa903bc-0789-43b2-901b-70d6c60334ba');
 
--- habilidades requeridas para un proyecto
 INSERT INTO proyectos_habilidades (id,id_proyecto,id_habilidad) 
 VALUES
 ('12c1ec3e-6d4b-4379-9322-195269bc5bd4','f660bbbf-dd1a-4eab-9866-dba8092c94c5','0463add9-313e-49bf-a07e-800612c36263'),
